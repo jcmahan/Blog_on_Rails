c4560faaa113a9a4393fafc9aa427f343d741d94
James Mahan <36861942+jcmahan@users.noreply.github.com>
2018-04-10 20:19:08 -0700

    "upvote/downvote counter fix"

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   get "/posts/new", to: "posts#new" 
   post "/posts", to: "posts#create"
   get "/posts/:id", to: "posts#show", as: :post 
-  get "/posts/:id/upvotes", to: "posts#upvote"
-  get "/posts/:id/downvotes", to: "posts#downvote"
+  get "/posts/:id/upvotes", to: "posts#upvote", as: :upvote
+  get "/posts/:id/downvotes", to: "posts#downvote", as: :downvote
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
