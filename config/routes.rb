20db8db6e5df76031fc4fbf35d3828d1f8cf6903
James Mahan <36861942+jcmahan@users.noreply.github.com>
2018-04-10 17:40:41 -0700

    "First view fixed"

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
 
-  resources :articles
-
-  root 'welcome#index'  
+  root "posts#index"  
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
